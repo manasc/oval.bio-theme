7abb1608cdf2285a3dc593cea9da020a176038a1
Fabian Acosta <acosahf@hotmail.com>
2020-09-25 11:17:17 -0500

    changes

---
+++ source/gutenberg-blocks/nmr-modular-block.php
@@ -0,0 +1,42 @@
+<div class="container">
+    <div class="grid grid-cols-2">
+
+
+        <div class="w-full md:w-1/2 px-8 relative">
+            <div class="w-full">
+                <div class="" style="padding-top: 100%"></div>
+            </div>
+            <div class="absolute z-0 " style="width: 55%; top: 50%; left:50%; transform: translate(50%,-50%)">
+                <div class="rounded-full bg-gray-400 border border-gray-400" style="padding-top: 100%"></div>
+            </div>
+            <div class="absolute z-0 " style="width: 25%; top: 50%; left:50%; transform: translate(10%,-200%)">
+                <div class="rounded-full bg-green-200 border border-gray-400" style="padding-top: 100%"></div>
+            </div>
+            <div class="absolute z-0" style="width: 30%; top: 50%; left:50%; transform: translate(300%,-200%)">
+                <div class="rounded-full bg-green-200 border border-gray-400" style="padding-top: 100%"></div>
+            </div>
+            <div class="absolute z-0" style="width: 40%; top: 50%; left:50%; transform: translate(200%,70%)">
+                <div class="rounded-full bg-green-200 border border-gray-400" style="padding-top: 100%"></div>
+            </div>
+            <div class="absolute z-0" style="width: 35%; top: 50%; left:50%; transform: translate(90%,45%)">
+                <div class="rounded-full bg-green-200 border border-gray-400" style="padding-top: 100%"></div>
+            </div>
+            <div class="absolute z-0" style="width: 38%; top: 50%; left:50%; transform: translate(-80%,40%)">
+                <div class="rounded-full bg-green-200 border border-gray-400" style="padding-top: 100%"></div>
+            </div>
+        </div>
+
+        <div class="mx-10">
+            <h1 class="text-4xl py-2">
+                The Pod is built for change.
+            </h1>
+            <p class="py-2">The pod at its measure dozens of the users health readings during every treatment session to ultimately create a detailed timeline map of the user and correlate event and life decisions/events (diet, exercise, traumatic events) to how they are represented in the data.</p>
+
+            <p class="py-2">The first pods will not have all the diagnostics or therapies that we plan for the first gen to have, as such we have designed the pod to let the user upgrade and swap out parts with as much ease as possible. So as the new diagnostics and therapies are made we can ship them to the pod owners and have them easily integrate them into their existing pod.</p>
+
+            <p class="py-2">This is part of the reason modularity is so important to us.</p>
+
+            <p class="py-2">The Pod technologies are divided into two main categories: Therapies and Diagnostics,</p>
+        </div>
+    </div>
+</div>
