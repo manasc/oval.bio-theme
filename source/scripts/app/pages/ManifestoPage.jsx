cda6f549c5a04a943991dff3f6325d11b5fcb84f
Manas Chimpidi <manaschimpidi@gmail.com>
2020-12-26 14:03:36 -0600

    fixed anchor links

--- source/scripts/app/pages/ManifestoPage.jsx
+++ source/scripts/app/pages/ManifestoPage.jsx
@@ -13,7 +13,18 @@ import TextSlider from "../sections/TextSlider";
 import TableOfContents from "../sections/TableOfContents";
 import LazyLoad from "react-lazyload";
 
-import ReactFullpage from "@fullpage/react-fullpage";
+const toc = (sections) => {
+    console.log("sections", sections);
+
+    let arr = {
+        title: "Table of Contents",
+        description:
+            "Ad totam voluptatibus facere voluptatem voluptas. Voluptatum quod quia qui qui repellat sed. Architecto impedit quisquam qui dolores repellendus consectetur. Quis nemo hic officia doloremque. Distinctio quia neque modi. Rerum perferendis ipsa similique beatae et et quas. Pariatur consectetur repudiandae quis nostrum. Sequi repellat qui doloribus molestias possimus adipisci. Laboriosam eius nihil ut sit assumenda placeat facere id. Iure maxime ut quam alias nostrum quisquam molestiae. Id esse aliquid natus at doloribus ducimus. Asperiores ad minus numquam et qui praesentium quos facere vel. Beatae eum dolores eos alias. Ullam qui ut ipsam minima accusamus. Culpa dicta quis porro voluptas quia praesentium quia. Dolorum voluptas esse repellat eos dolorem.",
+        links: [],
+    };
+
+    return sections.map((section) => arr.links.push({ name: section.title, link: section.link }));
+};
 
 const sections = [
     {
@@ -21,7 +32,7 @@ const sections = [
         title: "Title",
         slug: "title",
         component: (props) => <TextSlider {...props} />,
-        props: {},
+        props: () => {},
     },
     {
         id: 28056,
@@ -36,7 +47,9 @@ const sections = [
                 links: [],
             };
 
-            return sections.map((section) => arr.links.push({ name: section.title, link: section.link }));
+            sections.forEach((section) => arr.links.push({ name: section.title, link: section.slug }));
+
+            return arr;
         },
     },
     {
@@ -44,75 +57,69 @@ const sections = [
         title: "Our Mission",
         slug: "our-mission",
         component: (props) => <WordCloudSection {...props} />,
-        props: {},
+        props: () => {},
     },
     {
         id: 34352,
         title: "Our Values",
         slug: "our-values",
         component: (props) => <DiagramSection {...props} />,
-        props: {},
+        props: () => {},
     },
     {
         id: 92778,
         title: "Five PillarsThe Five Pillars",
         slug: "five-pillars",
         component: (props) => <FivePillars {...props} />,
-        props: {},
+        props: () => {},
     },
     {
         id: 29252,
         title: "Built for Change",
         slug: "built-for-change",
         component: (props) => <BuiltForChange {...props} />,
-        props: {},
+        props: () => {},
     },
     {
         id: 69447,
         title: "What is the pod?",
         slug: "what-is-the-pod",
         component: (props) => <PodIntro {...props} />,
-        props: {},
+        props: () => {},
     },
     {
         id: 44895,
         title: "More than Oval.Bio",
         slug: "more-than-oval",
         component: (props) => <MoreThanOval {...props} />,
-        props: {},
+        props: () => {},
     },
     {
         id: 171,
         title: "What does this matter?",
         slug: "what-do-we-matter",
         component: (props) => <MiroBoardSection {...props} />,
-        props: {},
+        props: () => {},
     },
     {
         id: 24645,
         title: "Milestones",
         slug: "milestones",
         component: (props) => <Milestones {...props} />,
-        props: {},
+        props: () => {},
     },
 ];
 
 function ManifestoPage() {
     return (
-        <ReactFullpage
-            licenseKey="E70AAD3B-F4774CC5-864728AE-C7DBB3D8"
-            scrollingSpeed={1000}
-            navigation={true}
-            render={({ state, fullpageApi }) => (
-                <ReactFullpage.Wrapper>
-                    {sections.map((section) => (
-                        <div key={section.id} className="section h-full">
-                            <LazyLoad once>{section.component(section.props)}</LazyLoad>
-                        </div>
-                    ))}
-                </ReactFullpage.Wrapper>
-            )}
-        />
+        <div className={"w-full"}>
+            {sections.length > 0 &&
+                sections.map((section) => (
+                    <div key={section.id} className="min-h-screen" id={section.slug}>
+                        <LazyLoad once>{section.component(section.props())}</LazyLoad>
+                    </div>
+                ))}
+        </div>
     );
 }
 
--- source/scripts/app/sections/TableOfContents.jsx
+++ source/scripts/app/sections/TableOfContents.jsx
@@ -1,30 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
+import { Link } from "react-scroll";
 
-function MoreThanOval({ title, description, links = [] }) {
-    return (
-        <section className="h-screen overflow-hidden flex items-center justify-center">
-            <div className="w-full max-w-4xl">
-                <div className="h-full flex justify-center -mx-2">
-                    <div className="w-full max-w-xl flex-none flex flex-col px-2">
-                        <div className="flex-1 px-10 overflow-y-scroll max-h-full">
-                            <div className="text-5xl">{title}</div>
-                            <div className="text-base mt-5" dangerouslySetInnerHTML={{ __html: description }} />
-                        </div>
-                    </div>
-                    <div className="flex-1  px-2">
-                        <ul className="">
-                            {links &&
-                                links.map((link) => (
-                                    <li className="mb-2 border-t hover" key={link.name}>
-                                        <div className="py-2 text-xl">{link.name}</div>
-                                    </li>
-                                ))}
-                        </ul>
-                    </div>
+const TableOfContents = ({ title, description, links = [] }) => (
+    <section className="h-full flex items-center justify-center">
+        <div className="w-full h-auto max-w-5xl flex justify-center -mx-2">
+            <div className="w-full max-w-xl flex-none flex flex-col px-2">
+                <div className="flex-1 px-10 overflow-y-scroll max-h-full">
+                    <div className="text-5xl">{title}</div>
+                    <div className="text-base mt-5" dangerouslySetInnerHTML={{ __html: description }} />
                 </div>
             </div>
-        </section>
-    );
-}
+            <div className="flex-1  px-2">
+                <ul className="">
+                    {links &&
+                        links.map((link) => (
+                            <li className="mb-2 border-t hover" key={link.name}>
+                                <div className="py-2 text-xl cursor-pointer" style={{ transitionDuration: "200ms" }}>
+                                    <Link activeClass="font-bold text-ovalGreenDark" to={link.link} spy={true} smooth={true}>
+                                        {link.name}
+                                    </Link>
+                                </div>
+                            </li>
+                        ))}
+                </ul>
+            </div>
+        </div>
+    </section>
+);
 
-export default MoreThanOval;
+export default TableOfContents;
