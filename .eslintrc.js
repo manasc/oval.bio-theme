9bb2800c36ef7c943c01ea684167208dd7627e49
Manas Chimpidi <manaschimpidi@gmail.com>
2021-01-03 16:31:23 -0600

    anchors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,45 @@
 module.exports = {
-  'root': true,
-  'extends': ['eslint:recommended','plugin:react/recommended'],
-  'globals': {
-    'wp': true,
-  },
-  'env': {
-    'node': true,
-    'es6': true,
-    'amd': true,
-    'browser': true,
-    'jquery': true,
-  },
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
-      'globalReturn': true,
-      'generators': false,
-      'objectLiteralDuplicateProperties': false,
-      'experimentalObjectRestSpread': true,
+    root: true,
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    globals: {
+        wp: true,
     },
-    'ecmaVersion': 2017,
-    'sourceType': 'module',
-  },
-  'plugins': [
-    'import',
-    'react'
-  ],
-  'settings': {
-    'import/core-modules': [],
-    'import/ignore': [
-      'node_modules',
-      '\\.(coffee|scss|css|less|hbs|svg|json)$',
-    ],
-  },
-  'rules': {
-    'no-console': 0,
-    'quotes': ['warn', 'double'],
-    // 'comma-dangle': [
-    //   'error',
-    //   {
-    //     'arrays': 'always-multiline',
-    //     'objects': 'always-multiline',
-    //     'imports': 'always-multiline',
-    //     'exports': 'always-multiline',
-    //     'functions': 'ignore',
-    //   },
-    // ],
-  },
+    env: {
+        node: true,
+        es6: true,
+        amd: true,
+        browser: true,
+        jquery: true,
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+            globalReturn: true,
+            generators: false,
+            objectLiteralDuplicateProperties: false,
+            experimentalObjectRestSpread: true,
+        },
+        ecmaVersion: 2017,
+        sourceType: "module",
+    },
+    "react/display-name": [false],
+    plugins: ["import", "react"],
+    settings: {
+        "import/core-modules": [],
+        "import/ignore": ["node_modules", "\\.(coffee|scss|css|less|hbs|svg|json)$"],
+    },
+    rules: {
+        "no-console": 0,
+        quotes: ["warn", "double"],
+        // 'comma-dangle': [
+        //   'error',
+        //   {
+        //     'arrays': 'always-multiline',
+        //     'objects': 'always-multiline',
+        //     'imports': 'always-multiline',
+        //     'exports': 'always-multiline',
+        //     'functions': 'ignore',
+        //   },
+        // ],
+    },
 };
--- source/scripts/app/pages/ProductPage.jsx
+++ source/scripts/app/pages/ProductPage.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import LazyLoad from "react-lazyload";
+import { Link, Element } from "react-scroll";
+
 // section
 import ProductSection from "../sections/ProductSection";
 
@@ -18,6 +21,8 @@ import CostOfGoodsSection from "../sections/CostOfGoodsSection";
 
 import ChosenArticlesSection from "../sections/ChosenArticlesSection";
 import ArticleTeaserSection from "../sections/ArticleTeaserSection";
+
+import AnchorLinksNav from "../components/AnchorLinksNav";
 
 function ProductPage({ productId }) {
     const [productData, setProductData] = useState("");
@@ -46,149 +51,210 @@ function ProductPage({ productId }) {
         console.log(productData, productMeta);
     }, [productData, productMeta]);
 
+    const metaSections = [
+        {
+            id: 64242,
+            title: "Key Benefits",
+            slug: "key_benefits",
+            component: (props) => <KeyBenefitsSection {...props} />,
+            props: {
+                data: productMeta["key_benefits"],
+            },
+        },
+        {
+            id: 37416,
+            title: "How It Works",
+            slug: "how_it_works",
+            component: (props) => <HowItWorksSection {...props} />,
+            props: {
+                data: productMeta["how_it_works"],
+            },
+        },
+        {
+            id: 82917,
+            title: "Nutrition Ingredients",
+            slug: "nutrition_ingredients",
+            component: (props) => <NutritionIngredientsSection {...props} />,
+            props: {
+                data: productMeta["nutrition_ingredients"],
+            },
+        },
+        {
+            id: 44837,
+            title: "Key Benefits",
+            slug: "life_extension",
+            component: (props) => <LifeExtensionSection {...props} />,
+            props: {},
+        },
+        {
+            id: 85714,
+            title: "Components",
+            slug: "components",
+            component: (props) => <ComponentsSection {...props} />,
+            props: {
+                data: productMeta["components"],
+            },
+        },
+        {
+            id: 86390,
+            title: "Cost Of Goods",
+            slug: "cost_of_goods",
+            component: (props) => <CostOfGoodsSection {...props} />,
+            props: {
+                data: productMeta["cost_of_goods"],
+            },
+        },
+        {
+            id: 52629,
+            title: "Future Of Product",
+            slug: "future_of_product",
+            component: (props) => <ChosenArticlesSection {...props} />,
+            props: {
+                sectionTitle: "Product Future",
+                articles: (productMeta["future_of_product"] && productMeta["future_of_product"].articles) || [],
+            },
+        },
+        {
+            id: 76949,
+            title: "Pipeline",
+            slug: "pipeline",
+            component: (props) => <PipelineSection {...props} />,
+            props: {
+                data: productMeta["pipeline"],
+            },
+        },
+        {
+            id: 90508,
+            title: "Directions For Use",
+            slug: "directions_for_use",
+            component: (props) => <DirectionsForUseSection {...props} />,
+            props: {
+                data: productMeta["directions_for_use"],
+            },
+        },
+        {
+            id: 59574,
+            title: "Side Effects",
+            slug: "side_effects",
+            component: (props) => <TextSection {...props} />,
+            props: {
+                title: "Side Effects",
+                description: productMeta["side_effects"] && productMeta["side_effects"].risks_concerns,
+            },
+        },
+        {
+            id: 18489,
+            title: "Faq",
+            slug: "faq",
+            component: (props) => <FaqSection {...props} />,
+            props: {
+                data: productMeta["faq"],
+            },
+        },
+        {
+            id: 61194,
+            title: "Competitive Comparison",
+            slug: "competitive_comparison",
+            component: (props) => <ArticleTeaserSection {...props} />,
+            props: {
+                sectionTitle: "Product Future",
+                articleId: (productMeta["competitive_comparison"] && productMeta["competitive_comparison"].articles.ID) || [],
+            },
+        },
+        {
+            id: 75246,
+            title: "Experiments",
+            slug: "experiments",
+            component: (props) => <ChosenArticlesSection {...props} />,
+            props: {
+                sectionTitle: "Tests & Experiments",
+                articles: (productMeta["experiments"] && productMeta["experiments"].articles) || [],
+            },
+        },
+        {
+            id: 49325,
+            title: "Clinical Trials",
+            slug: "clinical_trials",
+            component: (props) => <ChosenArticlesSection {...props} />,
+            props: {
+                sectionTitle: "Clinical Trials",
+                articles: (productMeta["clinical_trials"] && productMeta["clinical_trials"].articles) || [],
+            },
+        },
+        {
+            id: 66022,
+            title: "Research And Development",
+            slug: "research_and_development",
+            component: (props) => <ChosenArticlesSection {...props} />,
+            props: {
+                sectionTitle: "Research And Development",
+                articles: (productMeta["research_and_development"] && productMeta["research_and_development"].articles) || [],
+            },
+        },
+        {
+            id: 92027,
+            title: "Quality Control",
+            slug: "quality_control",
+            component: (props) => <TextSection {...props} />,
+            props: {
+                title: "Quality Control",
+                description: (productMeta["quality_control"] && productMeta["quality_control"].description) || "",
+            },
+        },
+        {
+            id: 50505,
+            title: "Manufacturing Challenges",
+            slug: "manufacturing_challenges",
+            component: (props) => <TextSection {...props} />,
+            props: {
+                title: "Manufacturing Challenges",
+                description: (productMeta["manufacturing_challenges"] && productMeta["manufacturing_challenges"].description) || "",
+            },
+        },
+        {
+            id: 65379,
+            title: "Help Needed",
+            slug: "help_needed",
+            component: (props) => <CardPostingSection {...props} />,
+            props: {
+                title: "Help Needed",
+                cards: productMeta["help_needed"] && productMeta["help_needed"].technologies,
+            },
+        },
+        {
+            id: 22144,
+            title: "Opportunities",
+            slug: "opportunities",
+            component: (props) => <CardPostingSection {...props} />,
+            props: {
+                title: "Opportunities",
+                cards: productMeta["opportunities"] && productMeta["opportunities"].opportunity,
+            },
+        },
+    ];
+
     return (
         <React.Fragment>
+            <AnchorLinksNav
+                sections={metaSections.map(({ title, slug }) => {
+                    return { title, slug };
+                })}
+            />
             {productData && <ProductSection productData={productData} />}
             {productMeta && (
                 <div className="mx-auto max-w-6xl md:px-3">
-                    {productMeta["key_benefits"] && (
-                        <div className={marginBottom}>
-                            <KeyBenefitsSection data={productMeta["key_benefits"]} />
-                        </div>
-                    )}
-
-                    {productMeta["how_it_works"] && (
-                        <div className={marginBottom}>
-                            <HowItWorksSection data={productMeta["how_it_works"]} />
-                        </div>
-                    )}
-
-                    <div className={marginBottom}>
-                        <LifeExtensionSection />
-                    </div>
-
-                    {productMeta["nutrition_ingredients"] && (
-                        <div className={marginBottom}>
-                            <NutritionIngredientsSection data={productMeta["nutrition_ingredients"]} />
-                        </div>
-                    )}
-
-                    {productMeta["components"] && (
-                        <div className={marginBottom}>
-                            <ComponentsSection data={productMeta["components"]} />
-                        </div>
-                    )}
-
-                    {productMeta["cost_of_goods"] && (
-                        <div className={marginBottom}>
-                            <CostOfGoodsSection data={productMeta["cost_of_goods"]} />
-                        </div>
-                    )}
-
-                    {productMeta["future_of_product"] && (
-                        <div className={marginBottom}>
-                            <ChosenArticlesSection
-                                sectionTitle="Product Future"
-                                articles={productMeta["future_of_product"].articles || []}
-                            />
-                        </div>
-                    )}
-
-                    {productMeta["pipeline"] && (
-                        <div className={marginBottom}>
-                            <PipelineSection data={productMeta["pipeline"]} />
-                        </div>
-                    )}
-
-                    {productMeta["directions_for_use"] && (
-                        <div className={marginBottom}>
-                            <DirectionsForUseSection data={productMeta["directions_for_use"]} />
-                        </div>
-                    )}
-
-                    {productMeta["side_effects"] && (
-                        <div className={marginBottom}>
-                            <TextSection title="Side Effects" description={productMeta["side_effects"].risks_concerns} />
-                        </div>
-                    )}
-
-                    {productMeta["faq"] && (
-                        <div className={marginBottom}>
-                            <FaqSection data={productMeta["faq"]} />
-                        </div>
-                    )}
-
-                    {productMeta["competitive_comparison"] && (
-                        <div className={marginBottom}>
-                            <ArticleTeaserSection
-                                sectionTitle="Competitive Comparison"
-                                articleId={productMeta["competitive_comparison"].articles.ID}
-                            />
-                        </div>
-                    )}
-
-                    {productMeta["experiments"] && (
-                        <div className={marginBottom}>
-                            <ChosenArticlesSection
-                                sectionTitle="Tests & Experiments"
-                                articles={productMeta["experiments"].articles || []}
-                            />
-                        </div>
-                    )}
-
-                    {/* clinical trials */}
-                    {productMeta["clinical_trials"] && (
-                        <div className={marginBottom}>
-                            <ChosenArticlesSection
-                                sectionTitle="Clinical Trials"
-                                articles={productMeta["clinical_trials"].trial_articles || []}
-                            />
-                        </div>
-                    )}
-
-                    {productMeta["research_and_development"] && (
-                        <div className={marginBottom}>
-                            <ChosenArticlesSection
-                                sectionTitle="Research & Development"
-                                articles={productMeta["research_and_development"].articles || []}
-                            />
-                        </div>
-                    )}
-
-                    {productMeta["intsructables"] &&
-                        productMeta["intsructables"].articles.map((article, i) => (
-                            <div key={i} className={marginBottom}>
-                                <ArticleTeaserSection sectionTitle="Instructable" articleId={article.ID} />
-                            </div>
-                        ))}
-
-                    {productMeta["quality_control"] && (
-                        <div className={marginBottom}>
-                            <TextSection title="Quality Control" description={productMeta["quality_control"].description} />
-                        </div>
-                    )}
-
-                    {productMeta["manufacturing_challenges"] && (
-                        <div className={marginBottom}>
-                            <TextSection
-                                title="Manufacturing Challenges"
-                                description={productMeta["manufacturing_challenges"].description}
-                            />
-                        </div>
-                    )}
-
-                    {productMeta["help_needed"] && (
-                        <div className={marginBottom}>
-                            <CardPostingSection title="Help Needed" cards={productMeta["help_needed"].technologies} />
-                        </div>
-                    )}
-
-                    {productMeta["opportunities"] && (
-                        <div className={marginBottom}>
-                            <CardPostingSection title="Opportunities" cards={productMeta["opportunities"].opportunity} />
-                        </div>
-                    )}
-
+                    {productMeta &&
+                        metaSections.length > 0 &&
+                        metaSections.map(
+                            ({ id, component, props, slug }) =>
+                                productMeta[slug] && (
+                                    <Element key={id} className="mx-auto max-w-6xl md:px-3" name={slug}>
+                                        {/* <LazyLoad once> */}
+                                        <div className={marginBottom}>{component(props)}</div>
+                                        {/* </LazyLoad> */}
+                                    </Element>
+                                )
+                        )}
                 </div>
             )}
         </React.Fragment>
